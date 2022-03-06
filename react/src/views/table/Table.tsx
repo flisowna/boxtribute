0baa13964044f6bbfc6998a787c8e3702872f030
flisowna <ewa.agnieszka.flis@gmail.com>
2022-03-06 21:55:23 +0100

    WIP

--- react/src/views/table/Table.tsx
+++ react/src/views/table/Table.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { Box, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
-import { useTable } from 'react-table'
+import { Column, useTable } from 'react-table'
 
 const LOCATION_QUERY = gql`
 query Location($locationId: ID!) {
@@ -58,100 +58,119 @@ const Table = () => {
 
     const locationId = 1
 
-      const { loading, error, data } = useQuery(LOCATION_QUERY, {
+      const { loading, error, data: data2 } = useQuery(LOCATION_QUERY, {
     variables: {
       locationId,
     },
   });
 
+  type Cols = { col1: string; col2: string };
 
-    // const data = React.useMemo(
-    //     () => [
-    //       {
-    //         col1: 'Hello',
-    //         col2: 'World',
-    //       },
-    //       {
-    //         col1: 'react-table',
-    //         col2: 'rocks',
-    //       },
-    //       {
-    //         col1: 'whatever',
-    //         col2: 'you want',
-    //       },
-    //     ],
-    //     []
-    //   )
-    
-      const columns = React.useMemo(
-        () => [
+
+    const data = React.useMemo(
+        (): Cols[] => [
           {
-            Header: 'Column 1',
-            accessor: 'col1', // accessor is the "key" in the data
+            col1: 'Hello',
+            col2: 'World',
           },
           {
-            Header: 'Column 2',
-            accessor: 'col2',
+            col1: 'react-table',
+            col2: 'rocks',
+          },
+          {
+            col1: 'whatever',
+            col2: 'you want',
           },
         ],
         []
       )
     
-    //   const {
-    //     getTableProps,
-    //     getTableBodyProps,
-    //     headerGroups,
-    //     rows,
-    //     prepareRow,
-    //   } = useTable({ columns, data })
+      // const columns = React.useMemo(
+      //   () => [
+      //     {
+      //       Header: 'Column 1',
+      //       accessor: 'col1', // accessor is the "key" in the data
+      //     },
+      //     {
+      //       Header: 'Column 2',
+      //       accessor: 'col2',
+      //     },
+      //   ],
+      //   []
+      // )
+
+
+
+      const columns: Column<{ col1: string; col2: string }>[] = React.useMemo(
+        () => [
+          {
+            Header: "Column 1",
+            accessor: "col1" // accessor is the "key" in the data
+          },
+          {
+            Header: "Column 2",
+            accessor: "col2"
+          }
+        ],
+        []
+      );
+    
+    
+      const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data })
     console.log(data)
       return (
           
-        // <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-        //   <thead>
-        //     {headerGroups.map(headerGroup => (
-        //       <tr {...headerGroup.getHeaderGroupProps()}>
-        //         {headerGroup.headers.map(column => (
-        //           <th
-        //             {...column.getHeaderProps()}
-        //             style={{
-        //               borderBottom: 'solid 3px red',
-        //               background: 'aliceblue',
-        //               color: 'black',
-        //               fontWeight: 'bold',
-        //             }}
-        //           >
-        //             {column.render('Header')}
-        //           </th>
-        //         ))}
-        //       </tr>
-        //     ))}
-        //   </thead>
-        //   <tbody {...getTableBodyProps()}>
-        //     {rows.map(row => {
-        //       prepareRow(row)
-        //       return (
-        //         <tr {...row.getRowProps()}>
-        //           {row.cells.map(cell => {
-        //             return (
-        //               <td
-        //                 {...cell.getCellProps()}
-        //                 style={{
-        //                   padding: '10px',
-        //                   border: 'solid 1px gray',
-        //                   background: 'papayawhip',
-        //                 }}
-        //               >
-        //                 {cell.render('Cell')}
-        //               </td>
-        //             )
-        //           })}
-        //         </tr>
-        //       )
-        //     })}
-        //   </tbody>
-        // </table>
-      <>hallo</>
+        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th
+                    {...column.getHeaderProps()}
+                    style={{
+                      borderBottom: 'solid 3px red',
+                      background: 'aliceblue',
+                      color: 'black',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        style={{
+                          padding: '10px',
+                          border: 'solid 1px gray',
+                          background: 'papayawhip',
+                        }}
+                      >
+                        {cell.render('Cell')}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      // <>hallo</>
         )
 };
 
