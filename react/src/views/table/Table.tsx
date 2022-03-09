18a06b14803deb8db0dae3bd94eba453b25ee816
flisowna <ewa.agnieszka.flis@gmail.com>
2022-03-09 21:26:06 +0100

    add sorting and chakra styling to the react table

--- react/src/views/table/Table.tsx
+++ react/src/views/table/Table.tsx
@@ -2,34 +2,40 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { Box, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
-import { Column, useTable } from 'react-table'
+import { Column, useTable, useSortBy } from "react-table";
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
 
-const LOCATION_QUERY = gql`
-query Location($locationId: ID!) {
-  location(id: $locationId) {
-    id
-    name
-    isShop
-    boxState
-    boxes {
-      totalCount
-      elements {
-        product {
-          name
-          category {
-            name
+const BASE_QUERY = gql`
+  query Base($baseId: ID!) {
+    base(id: $baseId) {
+      locations {
+        id
+        name
+        isShop
+        boxState
+        boxes {
+          totalCount
+          elements {
+            id
+            product {
+              gender
+              name
+              sizes
+              category {
+                name
+              }
+              price
+            }
+            items
           }
-          price
         }
-        items
       }
     }
   }
-}
 `;
 
-//   
-
+//
 
 //   if (loading) return <div>Loading...</div>;
 //   if (error) return <div>Error: {JSON.stringify(error)}</div>;
@@ -53,100 +59,105 @@ query Location($locationId: ID!) {
 //     </Box>
 //   );
 type ProductRow = {
-  name: string,
-  price: number
-}
+  name: string;
+  id: number;
+  gender: string;
+  sizes: string;
+  items: number;
+};
 
 type UnterTableProps = {
-  tableData: ProductRow[]
-}
-
+  tableData: ProductRow[];
+};
 
 const UnterTable = (props: UnterTableProps) => {
+  const columns: Column<ProductRow>[] = React.useMemo(
+    () => [
+      {
+        Header: "Product",
+        accessor: "name", // accessor is the "key" in the data
+      },
+      {
+        Header: "Box ID",
+        accessor: "id",
+      },
+      {
+        Header: "Gender",
+        accessor: "gender",
+      },
+      {
+        Header: "Size",
+        accessor: "sizes",
+      },
+      {
+        Header: "Items",
+        accessor: "items",
+      },
+    ],
+    [],
+  );
 
-      const columns: Column<ProductRow>[] = React.useMemo(
-        () => [
-          {
-            Header: "Name",
-            accessor: "name" // accessor is the "key" in the data
-          },
-          {
-            Header: "Price",
-            accessor: "price"
-          }
-        ],
-        []
-      );
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data: props.tableData })
-    
-
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data: props.tableData,
+  }, useSortBy);
 
   return (
-          
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: 'solid 3px red',
-                  background: 'aliceblue',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
+    <Table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
               >
-                {column.render('Header')}
-              </th>
+                {column.render("Header")}
+                <chakra.span pl='4'>
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <TriangleDownIcon aria-label='sorted descending' />
+                    ) : (
+                      <TriangleUpIcon aria-label='sorted ascending' />
+                    )
+                  ) : null}
+                </chakra.span>
+              </Th>
             ))}
-          </tr>
+          </Tr>
         ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
                 return (
-                  <td
+                  <Td
                     {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'papayawhip',
-                    }}
                   >
-                    {cell.render('Cell')}
-                  </td>
-                )
+                    {cell.render("Cell")}
+                  </Td>
+                );
               })}
-            </tr>
-          )
+            </Tr>
+          );
         })}
-      </tbody>
-    </table>
-    )
-  
-}
+      </Tbody>
+    </Table>
+  );
+};
 
+const DataTable = () => {
+  const baseId = 1;
 
-
-const Table = () => {
-
-  const locationId = 1
-
-  const { loading, error, data: data2 } = useQuery(LOCATION_QUERY, {
+  const {
+    loading,
+    error,
+    data: data2,
+  } = useQuery(BASE_QUERY, {
     variables: {
-      locationId,
+      baseId,
     },
   });
   if (loading) {
@@ -157,24 +168,17 @@ const Table = () => {
     return <div>Error!</div>;
   }
 
-
-  console.log("data2", data2)
-  const data3 = data2?.location.boxes.elements.map(element => ({
+  const tableData = data2?.base.locations.flatMap((location) =>
+    location.boxes.elements.map((element) => ({
       name: element.product.name,
-      price: element.product.price
-    }
-  ));
-  
-  console.log("data3", data3)
+      id: element.id,
+      sizes: element.product.sizes,
+      gender: element.product.gender,
+      items: element.items,
+    })),
+  );
 
+  return <UnterTable tableData={tableData} />;
+};
 
-  return <UnterTable tableData={data3} />;
-
-
-}
-
-
-
-
-
-export default Table;
+export default DataTable;
