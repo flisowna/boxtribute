45b343566ff78ddd5c10c663695ee306fb9e3920
flisowna <ewa.agnieszka.flis@gmail.com>
2022-04-14 21:44:29 +0200

    WIP (box transfers form)

--- react/src/components/HeaderMenu.tsx
+++ react/src/components/HeaderMenu.tsx
@@ -114,6 +114,7 @@ const MenuLinks = ({ isOpen, onLinkClick, ...props }) => {
         <LoginOrUserMenuButton />
         <MenuItem to={`/bases/${baseId}/locations`} text="Locations" />
         <MenuItem to={`/bases/${baseId}/boxes`} text="Boxes" />
+        <MenuItem to={`/bases/${baseId}/transfers`} text="Transfers" />
       </Stack>
     </Box>
   );
--- react/src/views/transfers/Transfers.tsx
+++ react/src/views/transfers/Transfers.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
-import { Box, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Heading,
+  ListItem,
+  Select,
+  Stack,
+  UnorderedList,
+} from "@chakra-ui/react";
 import { Organisation, OrganisationsAndBasesQuery } from "generated/graphql";
-
-
-
 
 export const ORGANISATIONS_QUERY = gql`
   query OrganisationsAndBases {
@@ -20,26 +26,61 @@ export const ORGANISATIONS_QUERY = gql`
   }
 `;
 
-
 const OrganisationsList = () => {
-  const { loading, error, data } = useQuery<OrganisationsAndBasesQuery>(
-    ORGANISATIONS_QUERY,
-  );
+  const { loading, error, data } = useQuery<OrganisationsAndBasesQuery>(ORGANISATIONS_QUERY);
+  const [org, setOrg] = React.useState<Organisation | undefined>(undefined);
+
+  const handleOrganisationSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    
+    const selectedOrganisation = event.target.value;
+    const filteredOrganisation = data?.organisations?.find(organisation => 
+    organisation.id === selectedOrganisation)
+    setOrg(filteredOrganisation)
+    console.log(filteredOrganisation);
+  }
+  
+
+  const handleBaseSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedBase = event.target.value;
+    console.log(selectedBase);
+  };
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  console.log(data)
+  console.log(data);
 
-  const foo = data?.organisations?.[0]?.name
   return (
-      <>
-    Test: {foo}
+    <>
+      {/* to build filter which takes out from the dropdown the source organisation */}
+      <FormControl isRequired>
+        <FormLabel htmlFor="target organisation">Organisation</FormLabel>
+        <Select
+          onChange={handleOrganisationSelection}
+          id="organisation"
+          placeholder="Select organisation"
+        >
+          {data?.organisations?.map((organisation) => (
+            <option value={organisation.id} key={organisation.id}>
+              {organisation.name}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel htmlFor="target organisation">Bases</FormLabel>
+        <Select onChange={handleBaseSelection} id="base" placeholder="All">
+          {org?.bases?.map((base) => (
+            <option value={base.id} key={base.id}>
+              {base.name}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
     </>
-  )
-}
-  
-  
+  );
+};
+
 //   (
 //     <UnorderedList>
 //       {data?.?.map((name, i) => (
@@ -52,14 +93,12 @@ const OrganisationsList = () => {
 // };
 
 const Transfers = () => {
-    return (
+  return (
     <Box>
-
-      <Heading>Organisations</Heading>
-      <OrganisationsList/>
+      <Heading>Transfers</Heading>
+      <OrganisationsList />
     </Box>
   );
 };
-
 
 export default Transfers;
